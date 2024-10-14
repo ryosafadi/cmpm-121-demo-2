f5c836be4c33c2dd326ee00b5285a7842744f374
ryo <65832968+ryosafadi@users.noreply.github.com>
2024-10-14 10:16:28 -0700

    added title and canvas (step 1)

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,20 @@ const APP_NAME = "Sticker Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+//app.innerHTML = APP_NAME;
+
+const title = document.createElement("h1");
+title.innerHTML = APP_NAME;
+app.append(title);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.append(canvas);
+const ctx = canvas.getContext("2d");
+
+if (ctx) {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+else console.error("No 2D context found");
